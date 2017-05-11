c177a204433276bf469f66b27ef2bf8b4a63f28c
zhangxu31 <zhangxu31@zx.local>
2017-05-11 16:46:00 +0800

    tree demo

--- svg/subtree.vue
+++ svg/subtree.vue
@@ -1,4 +1,92 @@
 <template>
-  <div class="viewport treeclass" v-resize="resize">
+  <div>
   </div>
 </template>
+<script>
+  import * as d3 from 'd3';
+   const props = {
+     data: Object
+   }
+  export default {
+    props,
+    data () {
+      return {
+        internaldata: ''
+      }
+    },
+    mounted () {
+      const treeData = this.data;
+      const margin = {top: 20, right: 90, bottom: 30, left: 90},
+        width = 350 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+      const treemap = d3.tree()
+        .size([height, width]);
+
+      this.internaldata = {
+        treeData,
+        treemap
+      }
+      this.init();
+    },
+    methods: {
+      init(){
+        let margin = {top: 20, right: 90, bottom: 30, left: 90},
+          width = 660 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+        //  assigns the data to a hierarchy using parent-child relationships
+        let nodes = d3.hierarchy(this.internaldata.treeData, function(d) {
+          return d.children;
+          });
+
+        // maps the node data to the tree layout
+        nodes = this.internaldata.treemap(nodes);
+
+        // append the svg object to the body of the page
+        // appends a 'group' element to 'svg'
+        // moves the 'group' element to the top left margin
+        let svg = d3.select(this.$el).append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom),
+          g = svg.append("g")
+            .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+        // adds the links between the nodes
+        let link = g.selectAll(".link")
+          .data( nodes.descendants().slice(1))
+          .enter().append("path")
+          .attr("class", "link")
+          .attr("d", function(d) {
+            return "M" + d.y + "," + d.x
+            + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+            + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+            + " " + d.parent.y + "," + d.parent.x;
+            });
+
+        // adds each node as a group
+        let node = g.selectAll(".node")
+          .data(nodes.descendants())
+          .enter().append("g")
+          .attr("class", function(d) {
+            return "node" +
+            (d.children ? " node--internal" : " node--leaf"); })
+          .attr("transform", function(d) {
+            return "translate(" + d.y + "," + d.x + ")"; });
+
+        // adds the circle to the node
+        node.append("circle")
+          .attr("r", 10);
+
+        // adds the text to the node
+        node.append("text")
+          .attr("dy", ".35em")
+          .attr("x", function(d) { return d.children ? -13 : 13; })
+          .style("text-anchor", function(d) {
+          return d.children ? "end" : "start"; })
+          .text(function(d) { return d.data.name; });
+      }
+    }
+  }
+</script>
+
